b056fe5fb2e2833a617efa4713548959dca26c9a
kk0mtl <kk0mtl@navr.com>
2024-02-05 02:28:44 +0900

    first pomodoro commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xff232b55),
+          ),
+        ),
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xffe7626c),
+        ),
+        cardColor: const Color(0xfff4eddb),
+      ),
+      title: 'drawer',
+      home: const HomeScreen(),
+    );
+  }
+}
